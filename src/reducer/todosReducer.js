7770bb71c6e4594b09afc4eaecc183af386a221d
Rezwanu Haque Prince <rezwanul.haquebd777@gmail.com>
2022-11-12 13:26:02 -0800

    todos reducer complete

--- src/reducer/todosReducer.js
+++ src/reducer/todosReducer.js
@@ -1,3 +1,5 @@
+import { GET_TODOS_FAILED, GET_TODOS_REQUEST, GET_TODOS_SUCCESS } from "../constants/todoConstants";
+
 // state
 const initialState={
    isLoading:false,
@@ -6,5 +8,29 @@ const initialState={
 
 }
 
+const todosReduer=(state=initialState,action)=>{
+   switch (action.type) {
+      case GET_TODOS_REQUEST:
+         return{
+            ...state,
+            isLoading:true
+         }
+      case GET_TODOS_SUCCESS:
+         return{
+            isLoading:false,
+            todos:action.payload,
+            error:null
+         }
+      case GET_TODOS_FAILED:
+         return{
+            isLoading:false,
+            todos:[],
+            error:action.payload
+         }
+   
+      default:
+        return state;
+   }
+}
 
-
+export default todosReduer
